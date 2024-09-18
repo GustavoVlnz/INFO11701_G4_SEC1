ec77655154d020680c491b59c256ab57825320e1
Alex <acarrasco2024@alu.uct.cl>
2024-09-18 10:42:08 -0300

    footer - javascript

---
+++ Detalles-vista/vista_prestador.js
@@ -0,0 +1,3 @@
+    // Impedir que la descripción se pueda expandir con el mouse
+    document.getElementById('descripcion-servicio').style.resize = 'none';
+    document.getElementById('razon-eliminacion').style.resize = 'none';

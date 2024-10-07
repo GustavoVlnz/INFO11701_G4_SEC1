f5f3a26fef13ed2e60fce6e23f1e55b9217d285d
Alex <acarrasco2024@alu.uct.cl>
2024-10-06 23:06:23 -0300

    avance logica acceso roles

---
+++ Pagina/Logged/Clientes/Perfil/Seguridad/Autenticacion/Autenticacion.js
@@ -0,0 +1,24 @@
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
+// Inicializa EmailJS con tu User ID
+(function () {
+    emailjs.init('yznFfV3mCG_WiP22y'); // Sustituye por tu User ID de EmailJS
+})();
+
+function sendAuthCode() {
+    const email = document.getElementById('email').value;
+    const authCode = Math.floor(100000 + Math.random() * 900000); // Generar código de 6 dígitos
+
+    const templateParams = {
+        auth: authCode,
+        email: email,
+    };
+
+    // Envía el correo utilizando el servicio EmailJS
+    emailjs.send('service_vuwonco', 'template_9tuwxdc', templateParams)
+        .then(function(response) {
+            alert('Código de autenticación enviado a ' + email);
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    }
